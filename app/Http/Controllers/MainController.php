3fb2a7cfa17209af92a5f16d99c625203003c3bf
xanter30 <xanter30@gmail.com>
2018-09-11 22:05:20 +0300

    Initial commit

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class MainController extends Controller
+{
+    public function show($id)
+    {
+        return view('profile', ['user' => User::findOrFail($id)]);
+    }
+}
